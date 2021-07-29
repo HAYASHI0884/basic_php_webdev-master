b88eb4730f77041e4675c94e43795c0c3effcab2
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-07-29 16:12:17 +0900

    first commit

---
+++ work/web/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<?php
+  
+  require('../app/functions.php');
+
+  $names = [
+    "Taro", "Jiro", "Saburo",
+  ]
+?>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Practice</title>
+</head>
+<body>
+  <ul>
+    <?php if (empty($names)): ?>
+      <li>Nobady!</li>
+    <?php else: ?>
+      <?php foreach ($names as $name) :?>
+        <li><?= h($name); ?></li>
+      <?php endforeach; ?>
+    <?php endif; ?>
+  </ul>
+</body>
+</html>
