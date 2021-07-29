0a7880ffe2a299615f550c5837c7caf98f24d342
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-07-29 17:05:15 +0900

    ユーザー名の入力欄作成

--- work/web/index.php
+++ work/web/index.php
@@ -3,11 +3,12 @@
   require('../app/functions.php');
 
   include('../app/_parts/_header.php');
-  
+
 ?>
 
 <form action="result.php" method="get">
   <input type="text" name="message">
+  <input type="text" name="username">
   <button>Send</button>
 </form>
 
