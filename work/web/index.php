975998131e372bbe54fbe6d995b127e76adfa406
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-07-29 16:37:49 +0900

    result.phpファイルの作成

--- work/web/index.php
+++ work/web/index.php
@@ -1,24 +1,15 @@
-<!DOCTYPE html>
 <?php
   
   require('../app/functions.php');
 
   include('../app/_parts/_header.php');
-
-  $names = [
-    "Taro", "Jiro", "Saburo",
-  ]
+  
 ?>
 
-  <ul>
-    <?php if (empty($names)): ?>
-      <li>Nobady!</li>
-    <?php else: ?>
-      <?php foreach ($names as $name) :?>
-        <li><?= h($name); ?></li>
-      <?php endforeach; ?>
-    <?php endif; ?>
-  </ul>
+<form action="result.php" method="get">
+  <input type="text" name="message">
+  <button>Send</button>
+</form>
 
 <?php
 
